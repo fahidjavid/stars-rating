c2655e8948e03ebdc122f550713b8efd53b8f67e
Fahid Javid <fahidjavid@gmail.com>
2018-11-08 10:43:19 +0500

    Added ‘Avg Rating Display’ enable disable option.

--- admin/stars-rating-settings.php
+++ admin/stars-rating-settings.php
@@ -1,120 +1,175 @@
 <?php
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly.
+	exit; // Exit if accessed directly.
 }
 
 if ( ! class_exists( 'Stars_Rating_Settings' ) ) :
 
-    /**
-     * Class Stars_Rating_Settings
-     *
-     * Plugin's settings class
-     *
-     * @since 1.0.0
-     */
-    final class Stars_Rating_Settings {
+	/**
+	 * Class Stars_Rating_Settings
+	 *
+	 * Plugin's settings class
+	 *
+	 * @since 1.0.0
+	 */
+	final class Stars_Rating_Settings {
 
-        /**
-         * Single instance of Class.
-         *
-         * @var Stars_Rating_Settings
-         * @since 1.0.0
-         */
-        protected static $_instance;
+		/**
+		 * Single instance of Class.
+		 *
+		 * @var Stars_Rating_Settings
+		 * @since 1.0.0
+		 */
+		protected static $_instance;
 
-        /**
-         * Provides singleton instance.
-         *
-         * @since 1.0.0
-         */
-        public static function instance() {
-            if ( is_null( self::$_instance ) ) {
-                self::$_instance = new self();
-            }
-            return self::$_instance;
-        }
+		/**
+		 * Provides singleton instance.
+		 *
+		 * @since 1.0.0
+		 */
+		public static function instance() {
+			if ( is_null( self::$_instance ) ) {
+				self::$_instance = new self();
+			}
 
-        /**
-         * Stars_Rating_Settings constructor.
-         * @since 1.0.0
-         */
-        public function __construct() {
+			return self::$_instance;
+		}
 
-            $this->init_hooks();
+		/**
+		 * Stars_Rating_Settings constructor.
+		 * @since 1.0.0
+		 */
+		public function __construct() {
 
-            // Stars Rating plugin settings loaded action hook
-            do_action( 'Stars_Rating_Settings_loaded' );
+			$this->init_hooks();
 
-        }
+			// Stars Rating plugin settings loaded action hook
+			do_action( 'Stars_Rating_Settings_loaded' );
 
-        public function init_hooks() {
+		}
 
-            add_action( 'admin_init', array( $this, 'stars_rating_section' ) );
-            add_action( 'init', array( $this, 'update_settings_field' ) );
-        }
+		public function init_hooks() {
 
-        public function stars_rating_section() {
+			add_action( 'admin_init', array( $this, 'stars_rating_section' ) );
+			add_action( 'init', array( $this, 'update_settings_field' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_plugin_files' ) );
+		}
 
-            add_settings_section(
-                'stars_rating_section',
-                esc_html__( 'Stars Rating', 'stars-rating' ),
-                array( $this, 'stars_rating_section_callback' ),
-                'discussion'
-            );
+		public function stars_rating_section() {
 
-            add_settings_field(
-                'enabled_post_types',
-                esc_html__( 'Enabled Post Types', 'stars-rating' ),
-                array( $this, 'enabled_post_types_callback' ),
-                'discussion',
-                'stars_rating_section',
-                array(
-                    'enabled_post_types'
-                )
-            );
+			add_settings_section(
+				'stars_rating_section',
+				esc_html__( 'Stars Rating', 'stars-rating' ),
+				array( $this, 'stars_rating_section_callback' ),
+				'discussion'
+			);
 
-            // register enabled_posts field
-            register_setting( 'discussion','enabled_post_types', 'esc_attr' );
-        }
+			add_settings_field(
+				'enabled_post_types',
+				esc_html__( 'Enabled Post Types', 'stars-rating' ),
+				array( $this, 'enabled_post_types_callback' ),
+				'discussion',
+				'stars_rating_section',
+				array(
+					'enabled_post_types'
+				)
+			);
 
-        public function stars_rating_section_callback() {
-            echo '<p class="description">'. esc_html__( 'Check the post types on which you want to enable stars rating feature.', 'stars-rating' ) .'</p>';
-        }
+			add_settings_field( 'avg_rating_display',
+				esc_html__( 'Average Rating Above Comments section', 'stars-rating' ),
+				array( $this, 'avg_rating_display_callback' ),
+				'discussion',
+				'stars_rating_section',
+				array( 'avg_rating_display' ) );
 
-        public function enabled_post_types_callback( $args ) {
+			// register enabled_posts field
+			register_setting( 'discussion', 'enabled_post_types', 'esc_attr' );
 
-            $enabled_posts = get_option(' enabled_post_types' );
+			// register avg_rating_display field
+			register_setting( 'discussion', 'avg_rating_display', 'esc_attr' );
+		}
 
-            if ( ! is_array( $enabled_posts ) ) {
-                $enabled_posts = (array) $enabled_posts;
-            }
+		public function stars_rating_section_callback() {
+			echo '<p class="description">' . esc_html__( 'Check the post types on which you want to enable stars rating feature.', 'stars-rating' ) . '</p>';
+		}
 
-            $query = array(
-                'public' => true
-            );
+		public function enabled_post_types_callback( $args ) {
 
-            // get publicly registered post types
-            $post_types = get_post_types( $query, 'names' );
+			$enabled_posts = get_option( ' enabled_post_types' );
 
-            foreach ( $post_types  as $post_type ) {
+			if ( ! is_array( $enabled_posts ) ) {
+				$enabled_posts = (array) $enabled_posts;
+			}
 
-                $checked = in_array( $post_type, $enabled_posts ) ? 'checked="checked"' : '';
-                echo '<label for="'. $post_type .'"><input type="checkbox" id="'. $post_type .'" name="'. $args[0] .'[]" value="'. $post_type .'" '. $checked .'/>' . ucwords( $post_type ) . '</label><br>';
-            }
+			$query = array(
+				'public' => true
+			);
 
-        }
+			// get publicly registered post types
+			$post_types = get_post_types( $query, 'names' );
 
-        public function update_settings_field() {
-            add_filter( 'pre_update_option_enabled_post_types', array( $this, 'update_field_enabled_post_types' ), 10, 2 );
-        }
+			foreach ( $post_types as $post_type ) {
 
-        public function update_field_enabled_post_types( $new_value, $old_value ) {
-            $new_value = $_POST['enabled_post_types'];
-            return $new_value;
-        }
+				$checked = in_array( $post_type, $enabled_posts ) ? 'checked="checked"' : '';
+				echo '<label for="' . $post_type . '"><input type="checkbox" id="' . $post_type . '" name="' . $args[0] . '[]" value="' . $post_type . '" ' . $checked . '/>' . ucwords( $post_type ) . '</label><br>';
+			}
 
-    }
+		}
+
+		public function avg_rating_display_callback() {
+			$avg_rating_display = get_option( ' avg_rating_display', 'show' );
+
+			$avg_rating_show_status = 'checked';
+			$avg_rating_hide_status = 'unchecked';
+
+			if ( 'hide' == $avg_rating_display ) {
+				$avg_rating_show_status = 'unchecked';
+				$avg_rating_hide_status = 'checked';
+			}
+
+			echo '<label for="avg_rating_display"><input type="radio" id="avg_rating_display" name="avg_rating_display" value="show" ' . $avg_rating_show_status . ' />' . esc_html__( 'Show', 'stars-rating' ) . '</label>';
+			echo '<label for="avg_rating_display_no"><input type="radio" id="avg_rating_display_no" name="avg_rating_display" value="hide" ' . $avg_rating_hide_status . ' />' . esc_html__( 'Hide', 'stars-rating' ) . '</label>';
+		}
+
+		public function update_settings_field() {
+			add_filter( 'pre_update_option_enabled_post_types', array(
+				$this,
+				'update_field_enabled_post_types'
+			), 10, 2 );
+			add_filter( 'pre_update_option_avg_rating_display', array(
+				$this,
+				'update_field_avg_rating_display'
+			), 10, 2 );
+		}
+
+		public function update_field_enabled_post_types( $new_value, $old_value ) {
+			$new_value = $_POST['enabled_post_types'];
+
+			return $new_value;
+		}
+
+		public function update_field_avg_rating_display( $new_value, $old_value ) {
+			$new_value = $_POST['avg_rating_display'];
+
+			return $new_value;
+		}
+
+		public function enqueue_plugin_files() {
+
+			$plugin_url = WP_PLUGIN_URL;
+			$plugin_admin_url = $plugin_url . '/stars-rating/admin/';
+
+			// stars rating admin
+			wp_enqueue_style(
+				'stars-rating-admin',
+				$plugin_admin_url . 'css/stars-rating-admin.css',
+				array(),
+				'1.0.0'
+			);
+		}
+
+	}
 
 endif;
 
@@ -128,7 +183,7 @@ endif;
  * @return Stars_Rating_Settings
  */
 function Stars_Rating_Settings() {
-    return Stars_Rating_Settings::instance();
+	return Stars_Rating_Settings::instance();
 }
 
 // Get Stars_Rating_Settings Running.

b83b52661e9ad42e98e641f7481dbed8bbef1c16
Fahid Javid <fahidjavid@gmail.com>
2019-03-31 09:58:33 +0500

    Added JS controler to require rating when submitting a comment.

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,36 @@
 (function ($) {
-    $(window).load(function() {
+    $(window).load(function () {
         "use strict";
 
-        $('#rate-it').barrating({
-            theme: 'fontawesome-stars',
-            initialRating: 5
-        });
+        var ratingField = $('#rate-it');
+
+        if (ratingField.hasClass('require-yes')) {
+            var parentForm = ratingField.closest('form');
+            var submitButton = parentForm.find('input[type="submit"]');
+            ratingField.barrating({
+                theme: 'fontawesome-stars',
+                initialRating: 5,
+                onSelect: function (value, text, event) {
+                    if (event !== null && event.type === 'click') {
+                        console.log("You can go ahead");
+                        submitButton.addClass('reviewed');
+                    }
+                }
+            });
+
+            parentForm.on('submit', function () {
+                if (!submitButton.hasClass('reviewed')) {
+                    alert('Please select a rating.')
+                    return false;
+                }
+            });
+        } else {
+            ratingField.barrating({
+                theme: 'fontawesome-stars',
+                initialRating: 5
+            });
+
+        }
 
     });
 })(jQuery);

acb96b4db7dffc12c0e0d2df518785c266caac63
Fahid Javid <fahidjavid@gmail.com>
2019-04-09 10:38:17 +0500

    Added custom links.

--- admin/stars-rating-settings.php
+++ admin/stars-rating-settings.php
@@ -110,6 +110,15 @@ if ( ! class_exists( 'Stars_Rating_Settings' ) ) :
 				array( 'google_search_stars' )
 			);
 
+			add_settings_field(
+				'donation_link',
+				esc_html__( 'Donate "Stars Rating" And Similar OpenSource Projects!', 'stars-rating' ),
+				array( $this, 'donation_link_callback' ),
+				'discussion',
+				'stars_rating_section',
+				array( 'donation_link' )
+			);
+
 			// register enabled_posts field
 			register_setting( 'discussion', 'enabled_post_types', 'esc_attr' );
 
@@ -213,6 +222,10 @@ if ( ! class_exists( 'Stars_Rating_Settings' ) ) :
 
 			echo '<label for="google_search_stars_show"><input type="radio" id="google_search_stars_show" name="google_search_stars" value="show" ' . $google_search_stars_show . ' />' . esc_html__( 'Show', 'stars-rating' ) . '</label>';
 			echo '<label for="google_search_stars_hide"><input type="radio" id="google_search_stars_hide" name="google_search_stars" value="hide" ' . $google_search_stars_hide . ' />' . esc_html__( 'Hide', 'stars-rating' ) . '</label>';
+		}
+
+		public function donation_link_callback(){
+			echo '<div class="custom-links"><a class="donation-link" href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=fahidjavid%40gmail.com&item_name=OpenSource+Projects+Support&currency_code=USD&source=url" target="_blank">Donate Now!</a><i>OR</i><a class="review-link" href="https://wordpress.org/support/plugin/stars-rating/reviews/#new-post" target="_blank">Review Plugin!</a></div>';
 		}
 
 		public function update_settings_field() {

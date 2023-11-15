2b10f5752a42e834065b027fd126c5c60e1a53b8
Fahid Javid <fahidjavid@gmail.com>
2023-11-15 08:27:32 +0500

    Improved public and admin assets loading method.

--- includes/stars-rating-include.php
+++ includes/stars-rating-include.php
@@ -1,17 +1,26 @@
 <?php
 
+/**
+ * Load Admin Files.
+ */
+if ( is_admin() ) {
+	/**
+	 * The class responsible for stars rating plugin setting fields on the Discussions Settings page
+	 */
+	require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/stars-rating-settings.php';
+
+	/**
+	 * The class responsible for stars rating plugin meta-box field on the post/page edit/add page
+	 */
+	require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/stars-rating-metabox.php';
+}
 
 /**
- * The class responsible for stars rating plugin setting fields on the Discussions Settings page
+ * Load Public Files.
  */
-require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/stars-rating-settings.php';
-
-/**
- * The class responsible for stars rating plugin meta-box field on the post/page edit/add page
- */
-require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/stars-rating-metabox.php';
-
-/**
- * The class responsible for comments stars rating
- */
-require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/stars-rating-public.php';
+if ( ! is_admin() ) {
+	/**
+	 * The class responsible for comments stars rating
+	 */
+	require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/stars-rating-public.php';
+}

d40bfe102657c3ce0a6e12a2012f58c0cc5d08a4
Fahid Javid <fahidjavid@gmail.com>
2025-05-04 01:08:11 +0500

    Bumped plugin version from 4.0.5 to 4.0.6

--- stars-rating.php
+++ stars-rating.php
@@ -3,8 +3,8 @@
  * Plugin Name: Stars Rating
  * Plugin URI: https://wordpress.org/plugins/stars-rating/
  * Description: A plugin to turn comments into reviews by adding rating feature.
- * Version: 4.0.5
- * Tested up to: 6.7.1
+ * Version: 4.0.6
+ * Tested up to: 6.8.1
  * Requires at least: 5.0
  * Requires PHP: 7.4
  * Author: Fahid Javid
